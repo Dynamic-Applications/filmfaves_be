ee6c57a197e9ad4a54b33946f4a68c5c9146c5f2
Mirage Kamran <mirage.kamran.pkt@gmail.com>
2025-02-17 19:41:53 -0800

    token expiration reset back to 30min

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -64,7 +64,7 @@ function buildToken(user, roleName) {
         role_name: roleName,
     };
     const options = {
-        expiresIn: "10min",
+        expiresIn: "30min",
     };
     return jwt.sign(payload, process.env.JWT_SECRET, options);
 }
