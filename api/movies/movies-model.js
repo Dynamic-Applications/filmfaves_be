bfa05591cd7f7c52fa802c5b0bb0e3da3a7eb4a2
Mirage Kamran <mirage.kamran.pkt@gmail.com>
2025-03-23 11:20:06 -0700

    movies router and model updated

--- api/movies/movies-model.js
+++ api/movies/movies-model.js
@@ -1,25 +1,31 @@
 const db = require("../../config/db");
 
 const getAll = async () => {
-    return db.query("SELECT * FROM movies")
-}
+    return db.query("SELECT * FROM movies");
+};
 
 const getById = async (id) => {
-
     const result = await db.query("SELECT * FROM movies WHERE movie_id = $1", [
         id,
     ]);
-    return result.rows[0]; 
-}
+    return result.rows[0];
+};
 
-
-const addMovie = async (title, director, rate, genre, popular, description, image) => {
+const addMovie = async (
+    title,
+    director,
+    rate,
+    genre,
+    popular,
+    description,
+    image
+) => {
     return db.query(
         `INSERT INTO movies (title, director, rate, genre, popular, description, image)
         VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *`,
         [title, director, rate, genre, popular, description, image]
     );
-}
+};
 
 const updateMovie = async (
     movie_id,
@@ -47,8 +53,8 @@ const updateMovie = async (
 };
 
 const removeMovie = async (id) => {
-    return db.query('DELETE FROM movies WHERE movie_id = $1 RETURNING *', [id])
-}
+    return db.query("DELETE FROM movies WHERE movie_id = $1 RETURNING *", [id]);
+};
 
 module.exports = {
     getAll,
@@ -56,4 +62,4 @@ module.exports = {
     addMovie,
     updateMovie,
     removeMovie,
-}
+};
--- api/movies/movies-router.js
+++ api/movies/movies-router.js
@@ -2,21 +2,19 @@ const Movie = require("./movies-model");
 const router = require("express").Router();
 const { restricted } = require("../auth/auth-middleware");
 
-
-router.get("/", restricted, async(req, res) => {
-
+router.get("/", restricted, async (req, res) => {
     try {
-            const result = await Movie.getAll();
-            if (!result.rows || result.rows.length === 0) {
-                return res.status(404).json({ message: "No movies found." });
-            }
-            res.json(result.rows);
-        } catch (err) {
-            res.status(500).json({
-                message: `Failed to retrieve movies: ${err.message}`,
-            });
+        const result = await Movie.getAll();
+        if (!result.rows || result.rows.length === 0) {
+            return res.status(404).json({ message: "No movies found." });
         }
-})
+        res.json(result.rows);
+    } catch (err) {
+        res.status(500).json({
+            message: `Failed to retrieve movies: ${err.message}`,
+        });
+    }
+});
 
 router.get("/:id", restricted, async (req, res) => {
     try {
@@ -34,39 +32,48 @@ router.get("/:id", restricted, async (req, res) => {
         console.error(err.message);
         res.status(500).send("Server Error");
     }
-})
+});
 
 router.post("/", async (req, res) => {
     try {
-        const { title, director, rate, genre, popular, description, image } = req.body;
-        
-        const result = await Movie.addMovie(title, director, rate, genre, popular, description, image);
+        const { title, director, rate, genre, popular, description, image } =
+            req.body;
+
+        const result = await Movie.addMovie(
+            title,
+            director,
+            rate,
+            genre,
+            popular,
+            description,
+            image
+        );
 
         res.status(201).json(result.rows[0]);
-        
     } catch (err) {
         console.error(err.message);
         res.status(500).send("Server Error");
     }
-})
+});
 
 router.put("/:id", async (req, res) => {
-    const { title, director, rate, genre, popular, description, image } = req.body;
+    const { title, director, rate, genre, popular, description, image } =
+        req.body;
 
     try {
         const result = await Movie.updateMovie(
-                    req.params.id,
-                    title,
-                    director,
-                    rate,
-                    genre,
-                    popular,
-                    description,
-                    image
+            req.params.id,
+            title,
+            director,
+            rate,
+            genre,
+            popular,
+            description,
+            image
         );
-        
+
         if (result.rows.length === 0) {
-            return res.status(404).send("Movie not found")
+            return res.status(404).send("Movie not found");
         }
 
         res.json(result.rows[0]);
@@ -74,8 +81,7 @@ router.put("/:id", async (req, res) => {
         console.error(err.message);
         res.status(500).send("Server Error");
     }
-
-})
+});
 
 router.delete("/:id", async (req, res) => {
     const { id } = req.params;
@@ -94,12 +100,11 @@ router.delete("/:id", async (req, res) => {
             return res.status(404).send("Movie not found");
         }
 
-        res.send(`The movie "${movie.title}" has been deleted successfully.`)
+        res.send(`The movie "${movie.title}" has been deleted successfully.`);
     } catch (err) {
         console.error(err.message);
         res.status(500).send("Server Error");
-
     }
-})
+});
 
 module.exports = router;
